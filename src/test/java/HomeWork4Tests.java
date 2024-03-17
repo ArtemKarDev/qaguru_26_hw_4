7d6338ae8a2b312c451644e035286b1d15ce810c
ArtemKarDev <akm86@mail.ru>
2024-03-17 21:21:28 +0300

    comleted test

--- src/test/java/HomeWork4Tests.java
+++ src/test/java/HomeWork4Tests.java
@@ -1,17 +1,48 @@
+//package tests;
+
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.lang.module.Configuration;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
 
 public class HomeWork4Tests {
 
     @BeforeAll
     static void beforeAll(){
-        Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://github.com"
+        //Configuration.browserSize = "1720x880";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+        //Configuration.timeout = 5000; // default 400
     }
     @Test
     void gitHubSoftAssertions(){
+        open("/");
+        //$(data-target="qbsearch-input.hotkeyIndicator").click();
+        $(byText("Search or jump to...")).click();
+        $(".QueryBuilder-InputWrapper #query-builder-test").setValue("selenide").pressEnter();
+        $("div[data-testid='results-list']").$("div.search-title a").click();
+        $("a#wiki-tab").click();
+        $("input#wiki-pages-filter").setValue("SoftAssertions");
+        $("li.wiki-more-pages a[href='/selenide/selenide/wiki/SoftAssertions']").shouldHave(text("SoftAssertions")).click();
+        $("#wiki-wrapper").shouldHave(text("""
+            @ExtendWith({SoftAssertsExtension.class})
+            class Tests {
+                @Test
+                void test() {
+                    Configuration.assertionMode = SOFT;
+                    open("page.html");
+
+                    $("#first").should(visible).click();
+                    $("#second").should(visible).click();
+                }
+            }""")).shouldBe(visible);
 
 
     }
